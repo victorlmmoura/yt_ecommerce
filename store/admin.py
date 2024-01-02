31f4379cca46aba8ac7652d312e651ff7bda18f7
Victor Lelis <victorlm.moura@gmail.com>
2024-01-02 18:41:09 +0000

    registered admin models

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+
+from .models import Category, Product
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name','slug']
+    prepopulated_fields = {'slug': ('name',)}
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title','author','slug','price',
+                    'in_stock','created','update']
+    list_filter = ['in_stock','is_active']
+    list_editable = ['price','in_stock']
+    prepopulated_fields = {'slug': ('title',)}
