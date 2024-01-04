b47fb760c465046455abd1545bc671da01bcc4bc
Victor Lelis <victorlm.moura@gmail.com>
2024-01-04 23:45:41 +0000

    category list

--- store/views.py
+++ store/views.py
@@ -15,5 +15,7 @@ def product_detail(request, slug):
     product = get_object_or_404(Product, slug=slug, in_stock=True)
     return render(request, 'store/product/detail.html', {'product': product})
 
-def category_list(request, slug):
-    pass
+def category_list(request, category_slug):
+    category = get_object_or_404(Category, slug=category_slug)
+    products = Product.objects.filter(category=category)
+    return render(request, 'store/product/category.html',{'category':category, 'products':products})
